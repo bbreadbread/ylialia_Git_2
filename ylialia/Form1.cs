0aa0cca8fcd65c53d8b7afbbbe1dd556628a21a6
bbreadbread <breadtt00@gmail.com>
2025-02-27 11:56:46 +0500

    Реализация InitializeGraph

--- ylialia/Form1.cs
+++ ylialia/Form1.cs
@@ -2,11 +2,24 @@ namespace ylialia
 {
     public partial class Form1 : Form
     {
+        private Dictionary<string, Dictionary<string, int>> graph;
         public Form1()
         {
             InitializeComponent();
+            InitializeGraph();
         }
-
+        private void InitializeGraph()
+        {
+            graph = new Dictionary<string, Dictionary<string, int>>
+            {
+                { "A", new Dictionary<string, int> { { "B", 4 } } },
+                { "B", new Dictionary<string, int> { { "C", 6 }, { "D", 3 }, { "E", 6 } } },
+                { "C", new Dictionary<string, int>() },
+                { "D", new Dictionary<string, int> { { "E", 2 } } },
+                { "E", new Dictionary<string, int> { { "C", 4 }, { "F", 5 } } },
+                { "F", new Dictionary<string, int>() }
+            };
+        }
         private void btnFindPath_Click(object sender, EventArgs e)
         {
             Random random = new Random();
